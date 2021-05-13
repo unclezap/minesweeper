e136bfb57231166ef37fe2728d629c33fe1afed4
unclezap <zeb.blackwell@gmail.com>
2021-05-13 05:33:46 -0700

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_13_073524) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.integer "width"
     t.integer "height"
